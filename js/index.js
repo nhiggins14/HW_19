934055fa8d7aa7745eb08625bcf7824de05bffe0
Nathaniel <nathanielhiggins14@gmail.com>
2019-10-12 22:19:49 -0400

    Toggler
    toggle MOSTLY done

--- js/index.js
+++ js/index.js
@@ -1,89 +1,138 @@
 
-$('.L1').on('click', function () {
+$(document).on('click','.L1',function() {
     $('p.boxText').css('display','flex'),
     $('.C1').css('display','none'),
     $('.R1').css('display','none'),
     $('#rowCon1').addClass('rowContainerActive'),
     $('.L1').addClass('geoBlockActive'),
     $('.L1Title').addClass('titleActive')
-})
-// .on('click', function () {
-//     $('p.boxText').css('display','none'),
-//     $('.C1').css('display','block'),
-//     $('.R1').css('display','block'),
-//     $('#rowCon1').removeClass('rowContainerActive'),
-//     $('.L1').removeClass('geoBlockActive'),
-//     $('.L1Title').removeClass('titleActive')
-// })
-;
+}
+);
 
-$('.C1').on('click', function () {
+$(document).on('click','.geoBlockActive',function() {
+    $('p.boxText').css('display','none'),
+    $('.C1').css('display','block'),
+    $('.R1').css('display','block'),
+    $('#rowCon1').removeClass('rowContainerActive'),
+    $('.L1').removeClass('geoBlockActive'),
+    $('.L1Title').removeClass('titleActive')
+});
+
+
+$(document).on('click','.C1',function() {
     $('p.boxText').css('display','flex'),
     $('.L1').css('display','none'),
     $('.R1').css('display','none'),
     $('#rowCon1').addClass('rowContainerActive'),
     $('.C1').addClass('geoBlockActive'),
     $('.C1Title').addClass('titleActive')
-})
-$('.R1').on('click', function () {
+}
+);
+$(document).on('click','.geoBlockActive',function() {
+    $('p.boxText').css('display','none'),
+    $('.L1').css('display','block'),
+    $('.R1').css('display','block'),
+    $('#rowCon1').removeClass('rowContainerActive'),
+    $('.C1').removeClass('geoBlockActive'),
+    $('.C1Title').removeClass('titleActive')
+});
+
+
+$(document).on('click','.R1',function() {
     $('p.boxText').css('display','flex'),
     $('.L1').css('display','none'),
     $('.C1').css('display','none'),
     $('#rowCon1').addClass('rowContainerActive'),
     $('.R1').addClass('geoBlockActive'),
     $('.R1Title').addClass('titleActive')
-})
-// $('.geoBlockActive').on('click', function () {
-//     $('p.boxText').css('display','none'),
-//     $('.geoBlock').css('display','block'),
-//     $('#rowCon1').removeClass('rowContainerActive'),
-//     $('.geoBlock').removeClass('geoBlockActive'),
-//     $('.row1').removeClass('titleActive')
-// }
-// )
+}
+);
 
-$('.L3').on('click', function () {
-    // $('p.boxText').css('display','flex'),
+$(document).on('click','.geoBlockActive',function() {
+    $('p.boxText').css('display','none'),
+    $('.L1').css('display','block'),
+    $('.C1').css('display','block'),
+    $('#rowCon1').removeClass('rowContainerActive'),
+    $('.R1').removeClass('geoBlockActive'),
+    $('.R1Title').removeClass('titleActive')
+});
+
+
+// $('.C1').on('click', function () {
+//     $('p.boxText').css('display','flex'),
+//     $('.L1').css('display','none'),
+//     $('.R1').css('display','none'),
+//     $('#rowCon1').addClass('rowContainerActive'),
+//     $('.C1').addClass('geoBlockActive'),
+//     $('.C1Title').addClass('titleActive')
+// })
+// $('.R1').on('click', function () {
+//     $('p.boxText').css('display','flex'),
+//     $('.L1').css('display','none'),
+//     $('.C1').css('display','none'),
+//     $('#rowCon1').addClass('rowContainerActive'),
+//     $('.R1').addClass('geoBlockActive'),
+//     $('.R1Title').addClass('titleActive')
+// })
+
+
+
+$(document).on('click','.L3',function() {
     $('.C3').css('display','none'),
     $('.R3').css('display','none'),
-    $('.L3').css('background-image','none'),
+    // $('.L3').css('background-image','none'),
     $('#rowCon3').addClass('rowContainerActive'),
     $('.L3').addClass('geoAciveCaseStudy'),
-    $('.L3Title').addClass('titleActive')
-})
-;
+    $('.L3Title').addClass('titleBotActive')
+}
+);
 
-$('.C3').on('click', function () {
-    // $('p.boxText').css('display','flex'),
+$(document).on('click','.geoAciveCaseStudy',function() {
+    $('.C3').css('display','block'),
+    $('.R3').css('display','block'),
+    // $('.L3').css(' background-image', 'url(../images/Box\ L-3.svg)'),
+    $('#rowCon3').removeClass('rowContainerActive'),
+    $('.L3').removeClass('geoAciveCaseStudy'),
+    $('.L3Title').removeClass('titleBotActive')
+});
+
+
+
+$(document).on('click','.C3',function() {
     $('.L3').css('display','none'),
     $('.R3').css('display','none'),
-    $('.C3').css('background-image','none'),
+    // $('.L3').css('background-image','none'),
     $('#rowCon3').addClass('rowContainerActive'),
     $('.C3').addClass('geoAciveCaseStudy'),
-    $('.C3Title').addClass('titleActive')
-})
-$('.R3').on('click', function () {
-    // $('p.boxText').css('display','flex'),
+    $('.C3Title').addClass('titleBotActive')
+}
+);
+
+$(document).on('click','.geoAciveCaseStudy',function() {
+    $('.L3').css('display','block'),
+    $('.R3').css('display','block'),
+    // $('.L3').css(' background-image', 'url(../images/Box\ L-3.svg)'),
+    $('#rowCon3').removeClass('rowContainerActive'),
+    $('.C3').removeClass('geoAciveCaseStudy'),
+    $('.C3Title').removeClass('titleBotActive')
+});
+
+$(document).on('click','.R3',function() {
     $('.L3').css('display','none'),
     $('.C3').css('display','none'),
-    $('.R3').css('background-image','none'),
+    // $('.L3').css('background-image','none'),
     $('#rowCon3').addClass('rowContainerActive'),
     $('.R3').addClass('geoAciveCaseStudy'),
-    $('.R3Title').addClass('titleActive')
-  
-  
+    $('.R3Title').addClass('titleBotActive')
+}
+);
 
+$(document).on('click','.geoAciveCaseStudy',function() {
+    $('.L3').css('display','block'),
+    $('.C3').css('display','block'),
+    // $('.L3').css(' background-image', 'url(../images/Box\ L-3.svg)'),
+    $('#rowCon3').removeClass('rowContainerActive'),
+    $('.R3').removeClass('geoAciveCaseStudy'),
+    $('.R3Title').removeClass('titleBotActive')
+});
 
-})
-
-
-
-console.log('hello');
-
-
-//  <script>
-//    $('geoAciveCaseStudy').hover,function () {
-//             $('.R3').css('background-color', 'none')
-//          }
-          
-// </script>
